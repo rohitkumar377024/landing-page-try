82b8ed245f7f30082f7bd504a283704aa0ebb9a6
Rohit <rohitkumar377024@gmail.com>
2020-06-17 13:28:19 +0530

    Updates

---
+++ precache-manifest.ff952a70de166ce3c5f6f36a2fc962d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "132bb16085369dbc40c2155cee838b3a",
+    "url": "/landing-page-try/index.html"
+  },
+  {
+    "revision": "0f5cfd7cf79469ae416f",
+    "url": "/landing-page-try/static/css/main.475f8439.chunk.css"
+  },
+  {
+    "revision": "f03f23bce66c2d29d2b8",
+    "url": "/landing-page-try/static/js/2.39f19230.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/landing-page-try/static/js/2.39f19230.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f5cfd7cf79469ae416f",
+    "url": "/landing-page-try/static/js/main.d4985873.chunk.js"
+  },
+  {
+    "revision": "fdd5db721b976f7b7cf1",
+    "url": "/landing-page-try/static/js/runtime-main.b4be1c1e.js"
+  }
+]);
